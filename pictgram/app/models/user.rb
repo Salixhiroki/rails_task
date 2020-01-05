a9a1aea311e137bb457b434dea588d0ef01998f7
Tobita_Hiroki <salix.hiro@gmail.com>
2020-01-05 12:18:12 +0000

    task_submit

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -2,19 +2,22 @@ class User < ApplicationRecord
 
 validates :name, presence: true
 validates :email, presence: true
+
+# password => 長さを8以上32以下
 validates :password, length: { in: 8..32 }
 # validates :password_confirmation, length: { in: 8..32 }
 
+# パスワード=>アルファベット、数字それぞれ1文字以上含む
 VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
 validates :password, format: { with: VALID_PASSWORD_REGEX  }
 
 has_secure_password
 
+#email=> ~~@~~.~
+VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, format: { with: VALID_EMAIL_REGEX }
 
-VALID_EMAIL_REGEX =/[^\s]@[^\s]/
-validates :email, format: {with: VALID_EMAIL_REGEX}
-
-
+# name => 15文字以下
 validates :name, length: { maximum: 15 }
 
 end
