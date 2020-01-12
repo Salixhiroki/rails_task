90790c31424f18626300e5519ba18235e0d11f29
Tobita_Hiroki <salix.hiro@gmail.com>
2020-01-12 04:02:17 +0000

    rails07_submit

--- pictgram/app/controllers/favorites_controller.rb
+++ pictgram/app/controllers/favorites_controller.rb
@@ -2,9 +2,7 @@ class FavoritesController < ApplicationController
 
 
   def index
-  
     @favorite_topics=current_user.favorite_topics
-    
   end
 
   def create
@@ -14,18 +12,18 @@ class FavoritesController < ApplicationController
     @favorite.user_id=current_user.id
     # お気に入りを登録されるtopic_id
     @favorite.topic_id=params[:topic_id]
-    if favorite.save
+    if @favorite.save
       redirect_to topics_path, success: 'お気に入りを登録しました'
     else
-      redirect_to topics_path, danger: 'お気に入りの登録に失敗しました'
+      # redirect_to topics_path, danger: 'お気に入りの登録に失敗しました'
+      flash.now[:danger]='お気に入りの登録に失敗しました'
     end
-  
   end
   
   
   def cancel
     @favorite=Favorite.find_by(user_id: current_user.id,topic_id: params[:topic_id])
-    # @favorite.destroy
+    @favorite.destroy
     redirect_to topics_path, success: 'お気に入りを解除しました'
   end
   
